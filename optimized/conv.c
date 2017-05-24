a5f79a00626c959fbdda2cc341212d0ade346b47
etteerr <e.j.diepgrond@gmail.com>
2017-05-24 14:50:45 +0200

    Added boundry calculations

--- optimized/conv.c
+++ optimized/conv.c
@@ -12,37 +12,92 @@ double conv3(float** data, unsigned long width, unsigned long height, const floa
     data_t * outData = 0, *inData = *data;
 
     outData = aligned_alloc(32, width * height * sizeof (data_t));
-    
+
     tic();
-    
-    
-    register float kern __attribute__ ((__vector_size__ (32))); //register of size 8float
-    for(int i = 0; i<9; i++)
+
+
+    register float kern __attribute__ ((__vector_size__(32))); //register of size 8float
+    for (int i = 0; i < 9; i++)
         kern[i] = filter[i];
     register float kernl = filter[9];
-    register float acc1,acc2,acc3;
-    register float f1,f2,f3,f4,f5,f6;
+    register float acc1, acc2, acc3;
+    register float f1, f2, f3, f4, f5, f6;
 
-    for (int y = 1; y < height-1; y++) {
-        __builtin_prefetch(&get(y+1,0)); //prefetch next 4 lines (y+1 is used in this loop, should be preloaded already)
-        __builtin_prefetch(&get(y+2,0));
-        for (int x = 1; x < width-1; x++) {
-            //No prefetching here, we expect the CPU to predict this (Linear access)
-            f1 = get(x-1,y-1) * kern[0];
-            f2 = get(x-1,y  ) * kern[3];
-            f3 = get(x-1,y+1) * kern[6];
-            
-            f1 += get(x ,y-1) * kern[1];
-            f2 += get(x ,y  ) * kern[4];
-            f3 += get(x ,y+1) * kern[7];
-            
-            f1 += get(x+1,y-1) * kern[2];
-            f2 += get(x+1,y  ) * kern[3];
-            f3 += get(x+1,y+1) * kernl;
-            
-            set(x,y,f1+f2+f3);
-        }
+    //Edge case y==0
+    for (int x = 1; x < width - 1; x++) {
+        //No prefetching here, we expect the CPU to predict this (Linear access)
+        f1 = get(x - 1, 0) * kern[3];
+        f2 = get(x - 1, 1) * kern[6];
+
+        f3 = get(x, 0) * kern[4];
+        f1 += get(x,1) * kern[7];
+
+        f2 += get(x + 1, 0) * kern[3];
+        f3 += get(x + 1, 1) * kernl;
+
+        set(x, 0, f1 + f2 + f3);
     }
+
+    //middle
+    for (int y = 1; y < height - 1; y++) {
+        __builtin_prefetch(&get(y + 1, 0)); //prefetch next 4 lines (y+1 is used in this loop, should be preloaded already)
+        __builtin_prefetch(&get(y + 2, 0));
+
+        //Border x==0
+        f1 = get(0, y - 1) * kern[1];
+        f2 = get(0, y) * kern[4];
+        f3 = get(0, y + 1) * kern[7];
+
+        f1 += get(0 + 1, y - 1) * kern[2];
+        f2 += get(0 + 1, y) * kern[3];
+        f3 += get(0 + 1, y + 1) * kernl;
+
+        set(0, y, f1 + f2 + f3);
+
+        //middle
+        for (int x = 1; x < width - 1; x++) {
+            //No prefetching here, we expect the CPU to predict this (Linear access)
+            f1 = get(x - 1, y - 1) * kern[0];
+            f2 = get(x - 1, y) * kern[3];
+            f3 = get(x - 1, y + 1) * kern[6];
+
+            f1 += get(x, y - 1) * kern[1];
+            f2 += get(x, y) * kern[4];
+            f3 += get(x, y + 1) * kern[7];
+
+            f1 += get(x + 1, y - 1) * kern[2];
+            f2 += get(x + 1, y) * kern[3];
+            f3 += get(x + 1, y + 1) * kernl;
+
+            set(x, y, f1 + f2 + f3);
+        }
+
+        //BOrder x==width-1
+        f1 = get(width-1 - 1, y - 1) * kern[0];
+        f2 = get(width-1 - 1, y) * kern[3];
+        f3 = get(width-1 - 1, y + 1) * kern[6];
+
+        f1 += get(width-1, y - 1) * kern[1];
+        f2 += get(width-1, y) * kern[4];
+        f3 += get(width-1, y + 1) * kern[7];
+
+        set(width-1, y, f1 + f2 + f3);
+    }
+    //Border case y==height-1
+    for (int x = 1; x < width - 1; x++) {
+        //No prefetching here, we expect the CPU to predict this (Linear access)
+        f4 = get(x - 1, height - 2) * kern[0];
+        f5 = get(x - 1, height-1) * kern[3];
+
+        f6 = get(x, height - 2) * kern[1];
+        f4 += get(x, height - 1) * kern[4];
+
+        f5 += get(x + 1, height - 2) * kern[2];
+        f6 += get(x + 1, height - 1) * kern[5];
+
+        set(x, height-1, f4 + f5 + f6);
+    }
+
     double time = toc();
     free(inData);
     *data = outData;
